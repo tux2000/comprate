73cb6732354041b7079041d61cd1f2e08d2056b1
tux2000 <tux2000@users.noreply.github.com>
2023-07-19 12:43:45 +0200

    First working version

--- R/calculate_comp_rate.R
+++ R/calculate_comp_rate.R
@@ -1,3 +1,14 @@
-calculate_comp_rate <- function(date) {
+#' Calculate the comparison rate (jämförelseränta)
+#'
+#' @param date date
+#'
+#' @return comparison rate (double)
+#' @export
+#'
+#' @examples
+#' calculate_comp_rate(Sys.Date() + 89:91)
+#'
+#'
+calculate_comp_rate <- Vectorize(function(date) {
   general_rate_level(date) + 1
-}
+})
--- R/mortgage_bonds_addition.R
+++ R/mortgage_bonds_addition.R
@@ -1,3 +1,15 @@
-mortgage_bonds_addition <- function(date) {
+mortgage_bonds_addition <- function(date, ...) {
+  date <- as.Date(date)
+
+  ref_rates <- get_ref_rates(...)
+
+  df_diff <- ref_rates %>%
+    filter(.data[["Instype"]] == "BO") %>%
+    mutate(gov_bond = gov_bond_level(.data[["Maturity"]])) %>%
+    mutate(diff = .data[["Fixingrta"]] - .data[["gov_bond"]])
+
+  model <- stats::lm(diff ~ Maturity, data = df_diff)
+
+  stats::predict(model, data.frame(Maturity = date))
 
 }
